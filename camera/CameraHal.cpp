zzpianoman/android_device_samsung_tuna

camera: Forcefully add jpeg picture format to supported formats.
Also a few minor bugfixes/workarounds.
All camera apps I've tested (including AOSP and Google Camera) function well now.
Few issues left to fix, but this has been very successful so far.

Signed-off-by: Kyle Repinski <repinski23@gmail.com>

camera/CameraHal.cpp
@@ -4157,12 +4157,21 @@ void CameraHal::initDefaultParameters()
 
     // TI extensions for enable/disable algos
     // Hadcoded for now
+#ifdef OMAP_TUNA
+    p.set(TICameraParameters::KEY_ALGO_FIXED_GAMMA, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_NSF1, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_NSF2, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_SHARPENING, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_THREELINCOLORMAP, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_GIC, android::CameraParameters::FALSE);
+#else
     p.set(TICameraParameters::KEY_ALGO_FIXED_GAMMA, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_NSF1, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_NSF2, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_SHARPENING, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_THREELINCOLORMAP, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_GIC, android::CameraParameters::TRUE);
+#endif
 
     LOG_FUNCTION_NAME_EXIT;
 }

eMarco/hardware_ti_omap4

Conflicts:
	camera/CameraHal.cpp
	camera/OMXCameraAdapter/OMXCapabilities.cpp

camera/CameraHal.cpp
@@ -4617,12 +4617,21 @@ void CameraHal::initDefaultParameters()
 
     // TI extensions for enable/disable algos
     // Hadcoded for now
+#ifdef OMAP_TUNA
+    p.set(TICameraParameters::KEY_ALGO_EXTERNAL_GAMMA, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_NSF1, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_NSF2, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_SHARPENING, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_THREELINCOLORMAP, android::CameraParameters::FALSE);
+    p.set(TICameraParameters::KEY_ALGO_GIC, android::CameraParameters::FALSE);
+#else
     p.set(TICameraParameters::KEY_ALGO_EXTERNAL_GAMMA, android::CameraParameters::FALSE);
     p.set(TICameraParameters::KEY_ALGO_NSF1, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_NSF2, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_SHARPENING, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_THREELINCOLORMAP, android::CameraParameters::TRUE);
     p.set(TICameraParameters::KEY_ALGO_GIC, android::CameraParameters::TRUE);
+#endif
 
     LOG_FUNCTION_NAME_EXIT;
 }